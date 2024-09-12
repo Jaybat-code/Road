9163777631521fe8264f42de7911b5a41936f45e
Jaybat-code <jacobbattenfielddev@gmail.com>
2024-09-12 13:58:05 -0500

    Stopsign Working

---
+++ app.js
@@ -0,0 +1,61 @@
+const car = document.getElementById("car");
+const stopsign = document.getElementById("stopsign")
+const stopGo = document.getElementById("image");
+let stopSignThere = true
+let speed = 20;
+let carMoving = false;
+let laps =0
+
+
+if (!car.style.left) {
+    car.style.left = "-200px";
+}
+
+function stopSignLogic(){
+    stopsign.addEventListener("click",()=>{
+        if(stopSignThere){
+            stopGo.setAttribute("src","Assets/arrow.png");
+            stopSignThere=false;
+            speed = 20;
+            if(!carMoving){
+                carMoving=true;
+                moveCar();
+            }
+            moveCar();
+        }
+        else{
+            stopGo.setAttribute("src","Assets/stop.png");
+            stopSignThere=true;
+            lookForStop();
+        }
+    })
+}
+
+function lookForStop(){
+    if (parseInt(car.style.left)==1600 && stopSignThere){
+        speed=0;
+    }
+}
+
+function moveCar() {
+    let currentLeft = parseInt(car.style.left);
+    car.style.left = (currentLeft +speed) + "px";
+    if (currentLeft > window.innerWidth) {
+        car.style.left = "-200px";
+        laps++;
+        console.log(laps);
+    }
+    lookForStop();
+
+    if(speed>0||!stopSignThere){
+    requestAnimationFrame(moveCar);
+    }
+    else{
+        carMoving=false;
+    }
+}
+
+stopSignLogic();
+
+
+
